720234d674a329e59dda723bd17ff6ad3a55b667
Artem Molchanov <artem700@gmail.com>
2023-04-18 15:02:52 -0300

    added main

--- src/main/java/Radio.java
+++ src/main/java/Radio.java
@@ -1,2 +1,68 @@
-package PACKAGE_NAME;public class Radio {
+public class Radio {
+    public int currentStation;
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public void setCurrentStation(int newCurrentStation) {
+        if (newCurrentStation < 0) {
+
+            newCurrentStation = 9;
+        }
+        if (newCurrentStation > 9) {
+
+            newCurrentStation = 0;
+        }
+        currentStation = newCurrentStation;
+    }
+
+    public void setNextstation() {
+        int newStation = currentStation + 1;
+        setCurrentStation(newStation);
+    }
+
+    public void setPrevStation() {
+        int newStation = currentStation - 1;
+        setCurrentStation(newStation);
+    }
+
+
+    public void setCurrentStationDirectly(int newCurrentStation) {
+        if (newCurrentStation < 0) {
+            return;
+        }
+        if (newCurrentStation > 9) {
+            return;
+        }
+        currentStation = newCurrentStation;
+    }
+    public int currentVolumeLevel;
+
+    public int getCurrenVolumeLevel() {
+        return currentVolumeLevel;
+    }
+
+    public void setCurrentVolumeLevel(int newCurrentVolumeLevel) {
+        if (newCurrentVolumeLevel > 100) {
+
+            newCurrentVolumeLevel = 100;
+        }
+        if (newCurrentVolumeLevel < 0) {
+
+            newCurrentVolumeLevel = 0;
+        }
+        currentVolumeLevel = newCurrentVolumeLevel;
+    }
+
+    public void setNextVolumeLevel() {
+        int newLevel = currentVolumeLevel + 1;
+        setCurrentStation(newLevel);
+    }
+
+    public void setPrevVolumeLevel() {
+        int newLevel = currentVolumeLevel - 1;
+        setCurrentVolumeLevel(newLevel);
+    }
 }
+
--- src/test/java/RadioTest.java
+++ src/test/java/RadioTest.java
@@ -1,2 +1,115 @@
-package PACKAGE_NAME;public class RadioTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class RadioTest {
+
+    @Test
+
+    public void shouldSetMaxStationDirectly () {
+        Radio radio = new Radio();
+
+        radio.setCurrentStationDirectly(9);
+
+
+        int expected = 9;
+        int actual = radio.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void shouldSetMinStationDirectly () {
+        Radio radio = new Radio();
+
+        radio.setCurrentStationDirectly(0);
+
+
+        int expected = 0;
+        int actual = radio.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+
+    public void shouldNotSetAboveMaxStationDirectly() {
+        Radio radio = new Radio();
+
+        radio.setCurrentStationDirectly(10);
+
+        int expected = 0;
+        int actual = radio.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+
+    public void shouldNotSetUnderMinStationDirectly() {
+        Radio radio = new Radio();
+
+        radio.setCurrentStationDirectly(-1);
+
+        int expected = 0;
+        int actual = radio.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSetZeroAboveMaxViaButton() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(9);
+
+        radio.setNextstation();
+
+        int expected = 0;
+        int actual = radio.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void shouldSetNineUnderMinViaButton() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(0);
+
+        radio.setPrevStation();
+
+        int expected = 9;
+        int actual = radio.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void shouldNotSetVolumeAboveMaxViaButton() {
+        Radio radio = new Radio();
+        radio.setCurrentVolumeLevel(101);
+
+        radio.setNextVolumeLevel();
+
+        int expected = 100;
+        int actual = radio.getCurrenVolumeLevel();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotSetVolumeUnderMinViaButton() {
+        Radio radio = new Radio();
+        radio.setCurrentVolumeLevel(-1);
+
+        radio.setPrevVolumeLevel();
+
+        int expected = 0;
+        int actual = radio.getCurrenVolumeLevel();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
 }
